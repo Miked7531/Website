1dde733a2acbef2d7cc0c756fd3a5233b81b0999
Mike <miked7531@gmail.com>
2020-06-26 15:32:40 -0400

    fixed?

--- resume.js
+++ resume.js
@@ -1,10 +1,10 @@
 // These functions open and close the contact form 
 function openForm() {
-  document.getElementById("myform").style.display = "block";
+  document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
-  document.getElementById("myform").style.display = "none";
+  document.getElementById("myForm").style.display = "none";
   // this.close(); FIX
 }
 
