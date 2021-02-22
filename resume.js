3d4e269fa90185ab7204fc0652d3a1934f4e9c4a
Mike <miked7531@gmail.com>
2021-02-22 13:36:41 -0500

    needs more

--- resume.js
+++ resume.js
@@ -9,27 +9,39 @@ function closeForm() {
 }
 
 
-// This function covers the slide show
-var slideIndex = 1;
-  showSlides(slideIndex);
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// // This function covers the slide show
+// var slideIndex = 1;
+//   showSlides(slideIndex);
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+// 	if (n > slides.length) {slideIndex = 1}    
+// 	if (n < 1) {slideIndex = slides.length}
+// 	  for (i = 0; i < slides.length; i++) {
+// 	  slides[i].style.display = "none";  
+// 	  }
+// 	  for (i = 0; i < dots.length; i++) {
+// 		dots[i].className = dots[i].className.replace(" active", "");
+// 	  }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+// }
+//this function calculates my age, keeps it up to date
+
+var dob = '19921008';
+var year = Number(dob.substr(0, 4));
+var month = Number(dob.substr(4, 2)) - 1;
+var day = Number(dob.substr(6, 2));
+var today = new Date();
+var age = today.getFullYear() - year;
+if (today.getMonth() < month || (today.getMonth() == month && today.getDate() < day)) {
+  age--;
 }
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-	if (n > slides.length) {slideIndex = 1}    
-	if (n < 1) {slideIndex = slides.length}
-	  for (i = 0; i < slides.length; i++) {
-	  slides[i].style.display = "none";  
-	  }
-	  for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+document.getElementById("j").innerHTML = age;
